f962679eaa87494ada2b3aef5bd333050f2ece50
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2023-04-04 14:12:45 -0600

    Se implementó los servicios CRUD para la DB

--- FractionSpace/Game/urls.py
+++ FractionSpace/Game/urls.py
@@ -3,7 +3,13 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
+router.register(r'usuario', views.UsuarioViewSet)
+router.register(r'partida', views.PartidaViewSet)
+router.register(r'nivel', views.NivelViewSet)
+router.register(r'pregunta', views.PreguntaViewSet)
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+    path('api', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
 ]
--- FractionSpace/Game/views.py
+++ FractionSpace/Game/views.py
@@ -9,5 +9,18 @@ def index(request):
     return HttpResponse("Bienvenido a la página principal del videojuego FractionSpace.")
 
 class UsuarioViewSet(viewsets.ModelViewSet):
-    queryset = Usuario.objects.all()    # SELECT * FROM Usuario
+    queryset = Usuario.objects.all()        # SELECT * FROM Game_usuario
     serializer_class = UsuarioSerializer
+
+class PartidaViewSet(viewsets.ModelViewSet):
+    queryset = Partida.objects.all()        # SELECT * FROM Game_partida
+    serializer_class = PartidaSerializer
+
+class NivelViewSet(viewsets.ModelViewSet):
+    queryset = Nivel.objects.all()          # SELECT * FROM Game_nivel
+    serializer_class = NivelSerializer
+
+class PreguntaViewSet(viewsets.ModelViewSet):
+    queryset = Pregunta.objects.all()       # SELECT * FROM Game_pregunta
+    serializer_class = PreguntaSerializer
+ 
