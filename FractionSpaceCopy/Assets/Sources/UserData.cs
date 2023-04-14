42e6d32c63962ee0c4c76157c8678a8fceb34ce1
AntonioLaurance <74690107+AntonioLaurance@users.noreply.github.com>
2023-04-14 08:44:12 -0600

    Actualización de los códigos de comunicación con el servidor

---
+++ FractionSpaceCopy/Assets/Sources/UserData.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserData : MonoBehaviour
+{
+    // Datos del jugador
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    // For avoid lose of data when you change the scene
+    private void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
