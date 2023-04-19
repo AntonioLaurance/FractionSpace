610d4f6257f6678d830d5b546c1bcfa76f415639
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2023-04-19 11:52:20 -0600

    Nivel 2 con assets y prefabs

---
+++ FractionSpaceCopy/Assets/Sources/MenuPausa.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuPausa : MonoBehaviour
+{
+    public void Pausa()
+    {
+        Time.timeScale = 0f;
+    }
+}
