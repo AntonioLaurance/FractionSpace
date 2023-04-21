21bb66821a28ddc2fc200a78be5f39f17ed7215d
FranciscoSanchezz <117677307+FranciscoSanchezz@users.noreply.github.com>
2023-04-20 19:31:16 -0600

    Menu de pausa en el nivel 3 y mensajes emergentes para dar instrucciones

--- FractionSpaceCopy/Assets/Sources/InterfazDeUsuario.cs
+++ FractionSpaceCopy/Assets/Sources/InterfazDeUsuario.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class InterfazDeUsuario : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    #region CAMPOS
+    [Header("Mensaje Salteable (Cualquier tecla)")]
+    [SerializeField]
+    private GameObject mensajeSaltableObjeto;
+    [SerializeField]
+    private TMP_Text mensajeSaltableTexto;
+    private bool mensajeSaltableActivo;
+    #endregion
+
+    #region METODOS
+
+    private void Start()
     {
-        
+        mensajeSaltableActivo = true;
+        mensajeSaltableObjeto.SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnGUI()
     {
-        
+        if (mensajeSaltableActivo)
+        {
+            if (Input.anyKeyDown)
+            {
+                LimpiarMensajeTecla();
+            }
+        }
     }
+
+   /* public void MostrarMensajeSalteableTecla(string mensaje)
+    {
+        mensajeSaltableActivo = true;
+        mensajeSaltableTexto.text = mensaje;
+        mensajeSaltableObjeto.SetActive (true);
+    }
+   */
+    private void LimpiarMensajeTecla()
+    {
+        mensajeSaltableActivo = false;
+        mensajeSaltableObjeto.SetActive(false) ;
+    }
+
+    #endregion
 }
